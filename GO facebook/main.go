489ed124b745c89e593196690ef88bf222d63a9b
Caline <calineyammine123@gmail.com>
2022-02-20 22:25:04 +0200

    Inserting new user to database

--- GO facebook/main.go
+++ GO facebook/main.go
@@ -5,7 +5,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
-
+	"io/ioutil"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
 )
@@ -35,7 +35,7 @@ func main(){
 	router := mux.NewRouter()
 	router.HandleFunc("/users", getUsers).Methods("POST")
 	router.HandleFunc("/user/{id}", getUser).Methods("GET")
-	
+	router.HandleFunc("/signup",signUp).Methods("POST")
  	http.ListenAndServe(":8000", router)
 }
 
@@ -64,7 +64,7 @@ func getUsers(w http.ResponseWriter, r *http.Request) {
 
 	}
 	json.NewEncoder(w).Encode(users)
-  }
+}
 
 func getUser(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
@@ -83,3 +83,29 @@ func getUser(w http.ResponseWriter, r *http.Request) {
 	}
 	json.NewEncoder(w).Encode(user)
 }
+
+func signUp(w http.ResponseWriter, r *http.Request) {  
+	
+	stmt, err := db.Prepare("INSERT INTO users(user_name,user_email,password) VALUES (?,?,?)")
+	  if err != nil {
+		panic(err.Error())
+	  }  
+	  body, err := ioutil.ReadAll(r.Body)
+	  if err != nil {
+		panic(err.Error())
+	  }
+	  body_str:=string(body)
+	  fmt.Println(body_str)
+	  keyVal := make(map[string]string)
+	  json.Unmarshal(body, &keyVal)
+	  user_name := keyVal["user_name"]
+	  user_email := keyVal["user_email"]
+	  password := keyVal["password"]
+	  
+	  _, err = stmt.Exec(user_name, user_email, password)
+	
+	  if err != nil {
+		panic(err.Error())
+	  }  
+	  fmt.Fprintf(w, "User added")
+} 
